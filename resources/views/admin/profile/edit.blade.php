9ebfeb6113ebedf9a6796e60afe4e076d834e5b1
小林ありか <a.sapphire.21@gmail.com>
2019-09-16 11:02:05 +0000

    kadai

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>Create</title>
+        <title>Edit</title>
     </head>
     <body>
-        <h1>Create</h1>
+        <h1>Edit</h1>
     </body>
 </html>
