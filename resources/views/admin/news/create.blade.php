04ad61c41efde7af9f93225a755ffc25e759b50e
小林ありか <a.sapphire.21@gmail.com>
2019-10-24 17:51:57 +0000

    課題

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -38,6 +38,8 @@
                             <input type="file" class="form-control-file" name="image">
                         </div>
                     </div>
+                     {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
                 </form>
             </div>
         </div>
